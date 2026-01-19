cc2ce5330490692ed2110a0494dedf07f1bea4ea
Bett Lenox <keringlenox@gmail.com>
2026-01-19 20:52:11 +0300

    fastapi

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 from typing import Union
 
 from fastapi import FastAPI
+from models import Base, engine
 
 app = FastAPI()
+
+# create tables on startup
+
+
+@app.on_event("startup")
+def create_tables():
+    Base.metadata.create_all(Bind=engine)
 
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+    return {"Duka FastAI": "1.0"}
---
+++ models.py
@@ -0,0 +1,133 @@
+from typing import List
+from typing import Optional
+from sqlalchemy import ForeignKey
+from sqlalchemy import String
+from sqlalchemy import DateTime
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import relationship,sessionmaker
+from datetime import datetime
+
+from sqlalchemy import create_engine
+DATABASE_URL='sqlite:///mysales.db'
+
+
+engine = create_engine(
+    DATABASE_URL
+)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
+# ---------------- BASE ----------------
+class Base(DeclarativeBase):
+    pass
+
+
+# ---------------- PRODUCT ----------------
+class Product(Base):
+    __tablename__ = "Products"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(100), nullable=False)
+    price: Mapped[String] = mapped_column(String, nullable=False)
+
+    # Relationships
+    sales: Mapped[List["Sale"]] = relationship(
+        back_populates="product",
+        cascade="all, delete-orphan"
+    )
+
+    purchases: Mapped[List["Purchase"]] = relationship(
+        back_populates="product",
+        cascade="all, delete-orphan"
+    )
+
+    # Serializer
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "price": self.price
+        }
+
+
+# ---------------- SALE ----------------
+class Sale(Base):
+    __tablename__ = "Sales"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    product_id: Mapped[int] = mapped_column(
+        ForeignKey("Products.id"),
+        nullable=False
+    )
+    quantity: Mapped[String] = mapped_column(String, nullable=False)
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime,
+        default=datetime.utcnow
+    )
+
+    # Relationship
+    product: Mapped["Product"] = relationship(back_populates="sales")
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "product_id": self.product_id,
+            "product_name": self.product.name if self.product else None,
+            "quantity": self.quantity,
+            # "created_at": self.created_at.isoformat() if self.created_at else None
+        }
+
+
+# ---------------- PURCHASE ----------------
+class Purchase(Base):
+    __tablename__ = "Purchases"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    product_id: Mapped[int] = mapped_column(
+        ForeignKey("Products.id"),
+        nullable=False
+    )
+    quantity: Mapped[String] = mapped_column(String, nullable=False)
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime,
+        default=datetime.utcnow
+    )
+
+    # Relationship
+    product: Mapped["Product"] = relationship(back_populates="purchases")
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "product_id": self.product_id,
+            "product_name": self.product.name if self.product else None,
+            "quantity": self.quantity,
+            # "created_at": self.created_at.isoformat() if self.created_at else None
+        }
+
+
+# ---------------- USER ----------------
+class User(Base):
+    __tablename__ = "Users"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(String(80), unique=True, nullable=False)
+    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    password: Mapped[str] = mapped_column(String(120), nullable=False)
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime,
+        default=datetime.utcnow
+    )
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "username": self.username,
+            "email": self.email
+        }
